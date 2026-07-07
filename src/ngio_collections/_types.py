8b43accebeae59f3169a2c3d3625a786ac887fe1
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-07-07 11:21:16 +0200

    Type checking

--- src/ngio_collections/_types.py
+++ src/ngio_collections/_types.py
@@ -1,6 +1,6 @@
 from typing import TypeAlias
 
 JSONValue: TypeAlias = (
-    dict[str, "JSONType"] | list["JSONType"] | str | int | float | bool | None
+    dict[str, "JSONType"] | list["JSONType"] | str | int | float | bool | None  # ty:ignore[unresolved-reference]
 )
 """Type of a JSON document."""
